d2a14daaf7768b0908ddafbcea831884190748ec
Raman Shekhawat <raman.s@parity.io>
2026-07-21 01:32:34 +0530

    fix gitconfig of docs

--- apps/docs/lib/shared.ts
+++ apps/docs/lib/shared.ts
@@ -4,7 +4,7 @@ export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
 
 export const gitConfig = {
-  user: 'use-truapi',
+  user: 'justraman',
   repo: 'use-truapi',
   branch: 'main',
 };
